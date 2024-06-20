26792b202611491026d72706b70426240db0fd3c
Harshil Agrawal <harshil@cloudflare.com>
2024-06-20 15:02:32 +0200

    initial setup;

---
+++ lib/r2.ts
@@ -0,0 +1,20 @@
+import {
+    S3Client,
+    PutObjectCommand
+} from "@aws-sdk/client-s3";
+
+const ACCOUNT_ID = process.env.ACCOUNT_ID as string;
+const ACCESS_KEY_ID = process.env.ACCESS_KEY_ID as string;
+const SECRET_ACCESS_KEY = process.env.SECRET_ACCESS_KEY as string;
+
+
+const S3 = new S3Client({
+    region: "auto",
+    endpoint: `https://${ACCOUNT_ID}.r2.cloudflarestorage.com`,
+    credentials: {
+        accessKeyId: ACCESS_KEY_ID,
+        secretAccessKey: SECRET_ACCESS_KEY,
+    },
+});
+
+export default S3;
